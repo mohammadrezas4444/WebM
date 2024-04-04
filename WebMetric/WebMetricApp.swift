d2c69ad2ea13af76fe19989f2b714d0881e43aea
Mohammad Reza Sohrabi <katieharrison5551@gmail.com>
2024-04-04 14:39:33 +0330

    Initial Commit

---
+++ WebMetric/WebMetricApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebMetricApp.swift
+//  WebMetric
+//
+//  Created by Mohammad Reza Sohrabi on 2024-04-04.
+//
+
+import SwiftUI
+
+@main
+struct WebMetricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
